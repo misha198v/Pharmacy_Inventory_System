7df6b10ef88c3ed49f91389183cfdf2ac1609ac7
Kakyubya Misha <mishasarah198v@gmail.com>
2026-09-07 18:46:11 +0300

    Register admin model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Medicine
+from .models import InventoryLog, Medicine, UserProfile
 
 @admin.register(Medicine)
 class MedicineAdmin(admin.ModelAdmin):
@@ -7,3 +7,18 @@ class MedicineAdmin(admin.ModelAdmin):
     list_display = ('name', 'stock_quantity', 'expiry_date', 'status', 'days_until_expiry')
     list_filter = ('expiry_date',)
     search_fields = ('name',)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+    list_filter = ('role',)
+    search_fields = ('user__username', 'user__email')
+
+
+@admin.register(InventoryLog)
+class InventoryLogAdmin(admin.ModelAdmin):
+    list_display = ('medicine', 'action', 'performed_by', 'timestamp')
+    list_filter = ('action', 'timestamp')
+    search_fields = ('medicine__name', 'performed_by__username')
+    readonly_fields = ('timestamp',)
