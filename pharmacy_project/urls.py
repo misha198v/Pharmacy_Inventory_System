e432f9867d6cff2b80797b2a844bf981724d2bf9
Kakyubya Misha <mishasarah198v@gmail.com>
2026-09-24 13:18:26 +0300

    fix: remove the self-redirect on django root url

--- pharmacy_project/urls.py
+++ pharmacy_project/urls.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
-    path('', lambda request: redirect('http://localhost:3000/')),
+    # path('', lambda request: redirect('http://localhost:3000/')),
     path('admin/', admin.site.urls),
     path('api/', include('inventory.urls')), # Add this line for DRF's login/logout views
 ]
